0774858e2678b4053ddfca5b7d603d53646de58d
Maira Bello <maira.araujo@liferay.com>
2015-10-28 12:00:22 -0300

    Adds "data" to the ApiExplorer's ui

--- src/ApiExplorer.soy.js
+++ src/ApiExplorer.soy.js
@@ -44,7 +44,9 @@ Templates.ApiExplorer.title = function(opt_data, opt_ignored, opt_ijData) {
     }
   }
   var visible__soy261 = opt_data.visibility || ! (opt_data.visibility != null);
-  output += '<span class="explorer-title-name">' + soy.$$escapeHtml(opt_data.title ? opt_data.title : opt_data.path) + '</span><span class="explorer-visibility show-tooltip" alt="When your API is marked as \'visible\' it means that it can be requested by anyone, whereas \'invisible\' acts like a firewall where only your server can request it"><span class="explorer-visibility-icon ' + soy.$$escapeHtmlAttribute(visible__soy261 ? 'icon-12-check' : 'icon-12-close-short') + '"></span><span class="explorer-visibility-label">' + soy.$$escapeHtml(visible__soy261 ? 'Visible' : 'Invisible') + '</span></span></p>';
+  output += '<span class="explorer-title-name">' + soy.$$escapeHtml(opt_data.title ? opt_data.title : opt_data.path) + '</span><span class="explorer-visibility show-tooltip" alt="When your API is marked as \'visible\' it means that it can be requested by anyone, whereas \'invisible\' acts like a firewall where only your server can request it"><span class="explorer-visibility-icon ' + soy.$$escapeHtmlAttribute(visible__soy261 ? 'icon-12-check' : 'icon-12-close-short') + '"></span><span class="explorer-visibility-label">' + soy.$$escapeHtml(visible__soy261 ? 'Visible' : 'Invisible') + '</span></span>';
+  var dataEnabled__soy269 = opt_data.data || ! (opt_data.data != null);
+  output += '<span class="explorer-data show-tooltip"><span class="explorer-data-icon ' + soy.$$escapeHtmlAttribute(dataEnabled__soy269 ? 'icon-12-check' : 'icon-12-close-short') + '"></span><span class="explorer-data-label">Data</span></span></p>';
   return soydata.VERY_UNSAFE.ordainSanitizedHtml(output);
 };
 if (goog.DEBUG) {
@@ -79,19 +81,19 @@ Templates.ApiExplorer.auth = function(opt_data, opt_ignored, opt_ijData) {
   if (opt_data.auth && opt_data.auth.roles && opt_data.auth.roles.length) {
     output += '<div id="' + soy.$$escapeHtmlAttribute(opt_data.id) + '-auth"><p class="api-section-title">Authentication</p><div class="explorer-section-auth">';
     if (opt_data.auth.roles) {
-      var roleList283 = opt_data.auth.roles;
-      var roleListLen283 = roleList283.length;
-      for (var roleIndex283 = 0; roleIndex283 < roleListLen283; roleIndex283++) {
-        var roleData283 = roleList283[roleIndex283];
-        output += '<span class="label label-primary"><span class="label-icon icon-12-person"></span> ' + soy.$$escapeHtml(roleData283) + '</span>';
+      var roleList287 = opt_data.auth.roles;
+      var roleListLen287 = roleList287.length;
+      for (var roleIndex287 = 0; roleIndex287 < roleListLen287; roleIndex287++) {
+        var roleData287 = roleList287[roleIndex287];
+        output += '<span class="label label-primary"><span class="label-icon icon-12-person"></span> ' + soy.$$escapeHtml(roleData287) + '</span>';
       }
     }
     if (opt_data.auth.permissions) {
-      var permissionList290 = opt_data.auth.permissions;
-      var permissionListLen290 = permissionList290.length;
-      for (var permissionIndex290 = 0; permissionIndex290 < permissionListLen290; permissionIndex290++) {
-        var permissionData290 = permissionList290[permissionIndex290];
-        output += '<span class="label label-success"><span class="label-icon icon-12-check"></span> ' + soy.$$escapeHtml(permissionData290) + '</span>';
+      var permissionList294 = opt_data.auth.permissions;
+      var permissionListLen294 = permissionList294.length;
+      for (var permissionIndex294 = 0; permissionIndex294 < permissionListLen294; permissionIndex294++) {
+        var permissionData294 = permissionList294[permissionIndex294];
+        output += '<span class="label label-success"><span class="label-icon icon-12-check"></span> ' + soy.$$escapeHtml(permissionData294) + '</span>';
       }
     }
     output += '</div></div>';
@@ -114,11 +116,11 @@ Templates.ApiExplorer.params = function(opt_data, opt_ignored, opt_ijData) {
   var output = '';
   if (opt_data.parameters && opt_data.parameters.length) {
     output += '<div id="' + soy.$$escapeHtmlAttribute(opt_data.id) + '-params"><p class="api-section-title">Parameters</p><div class="explorer-section-params"><table class="table"><thead><tr><th>Name</th><th>Description</th><th class="text-center">Type</th><th class="text-center">Value</th><th class="text-center">Required</th></tr></thead><tbody>';
-    var paramList302 = opt_data.parameters;
-    var paramListLen302 = paramList302.length;
-    for (var paramIndex302 = 0; paramIndex302 < paramListLen302; paramIndex302++) {
-      var paramData302 = paramList302[paramIndex302];
-      output += '<tr><td>' + soy.$$escapeHtml(paramData302.name) + '</td><td>' + soy.$$escapeHtml(paramData302.description ? paramData302.description : '') + '</td><td class="text-center">' + soy.$$escapeHtml(paramData302.type ? paramData302.type : '') + '</td><td class="text-center">' + soy.$$escapeHtml(paramData302.value ? paramData302.value : '') + '</td><td class="text-center">' + ((paramData302.required) ? '<span class="explorer-icon-required icon-16-confirm"></span>' : '') + '</td></tr>';
+    var paramList306 = opt_data.parameters;
+    var paramListLen306 = paramList306.length;
+    for (var paramIndex306 = 0; paramIndex306 < paramListLen306; paramIndex306++) {
+      var paramData306 = paramList306[paramIndex306];
+      output += '<tr><td>' + soy.$$escapeHtml(paramData306.name) + '</td><td>' + soy.$$escapeHtml(paramData306.description ? paramData306.description : '') + '</td><td class="text-center">' + soy.$$escapeHtml(paramData306.type ? paramData306.type : '') + '</td><td class="text-center">' + soy.$$escapeHtml(paramData306.value ? paramData306.value : '') + '</td><td class="text-center">' + ((paramData306.required) ? '<span class="explorer-icon-required icon-16-confirm"></span>' : '') + '</td></tr>';
     }
     output += '</tbody></table></div></div>';
   }
@@ -139,11 +141,11 @@ if (goog.DEBUG) {
 Templates.ApiExplorer.tryParams = function(opt_data, opt_ignored, opt_ijData) {
   var output = '<div id="' + soy.$$escapeHtmlAttribute(opt_data.id) + '-tryParams" class="row">';
   if (opt_data.parameters) {
-    var paramList324 = opt_data.parameters;
-    var paramListLen324 = paramList324.length;
-    for (var paramIndex324 = 0; paramIndex324 < paramListLen324; paramIndex324++) {
-      var paramData324 = paramList324[paramIndex324];
-      output += '<div class="col-md-3"><label for="explorer-param-' + soy.$$escapeHtmlAttribute(paramData324.name) + '" class="api-section-label">' + soy.$$escapeHtml(paramData324.name) + ' ' + ((paramData324.required) ? '<span class="explorer-icon-required">*</span>' : '') + '</label><input id="explorer-param-' + soy.$$escapeHtmlAttribute(paramData324.name) + '" name="' + soy.$$escapeHtmlAttribute(paramData324.name) + '" class="form-control explorer-section-try-param" type="text" autocomplete="off" placeholder="' + soy.$$escapeHtmlAttribute(paramData324.value ? paramData324.value : '') + '" data-index="' + soy.$$escapeHtmlAttribute(paramIndex324) + '" data-oninput="handleParamInput_" ></div>';
+    var paramList328 = opt_data.parameters;
+    var paramListLen328 = paramList328.length;
+    for (var paramIndex328 = 0; paramIndex328 < paramListLen328; paramIndex328++) {
+      var paramData328 = paramList328[paramIndex328];
+      output += '<div class="col-md-3"><label for="explorer-param-' + soy.$$escapeHtmlAttribute(paramData328.name) + '" class="api-section-label">' + soy.$$escapeHtml(paramData328.name) + ' ' + ((paramData328.required) ? '<span class="explorer-icon-required">*</span>' : '') + '</label><input id="explorer-param-' + soy.$$escapeHtmlAttribute(paramData328.name) + '" name="' + soy.$$escapeHtmlAttribute(paramData328.name) + '" class="form-control explorer-section-try-param" type="text" autocomplete="off" placeholder="' + soy.$$escapeHtmlAttribute(paramData328.value ? paramData328.value : '') + '" data-index="' + soy.$$escapeHtmlAttribute(paramIndex328) + '" data-oninput="handleParamInput_" ></div>';
     }
   }
   output += ((('' + opt_data.path).indexOf('/*') != -1) ? '<div class="col-md-3"><label for="explorer-param-wildcard" class="api-section-label">Wildcard</label><input id="explorer-param-wildcard" name="Wildcard" class="form-control explorer-section-try-param" type="text" autocomplete="off" data-oninput="handleWildcardInput_" ></div>' : '') + '</div>';
@@ -178,9 +180,9 @@ if (goog.DEBUG) {
  */
 Templates.ApiExplorer.tryIt = function(opt_data, opt_ignored, opt_ijData) {
   var output = '<div id="' + soy.$$escapeHtmlAttribute(opt_data.id) + '-tryIt" class="row">';
-  var methodItems__soy360 = opt_data.method ? opt_data.method : ['get'];
-  var selectedIndex__soy361 = opt_data.methodSelectedIndex ? opt_data.methodSelectedIndex : 0;
-  output += '<div class="col-md-12"><div class="input-group">' + ((methodItems__soy360.length > 1) ? '<div class="input-group-btn">' + soy.$$escapeHtml(Templates.Select.content({events: {selectedIndexChanged: opt_data.id + ':handleMethodSelectedIndexChanged_'}, id: opt_data.id + '-methodSelect', label: '', items: methodItems__soy360, buttonClass: 'btn btn-default btn-sm dropdown-select-group-left', hiddenFieldName: 'method', selectedIndex: selectedIndex__soy361}, null, opt_ijData)) + '</div>' : '<div class="input-group-addon explorer-section-try-method">' + soy.$$escapeHtml(methodItems__soy360[0]) + '</div>') + '<div class="input-inner-addon input-inner-addon-left"><button data-clipboard data-target="#explorer-section-try-input" class="explorer-section-try-icon btn-transparent icon-16-link" type="button"></button><input id="explorer-section-try-input" class="form-control" type="text" value="' + soy.$$escapeHtmlAttribute(opt_data.host ? opt_data.host : '') + soy.$$escapeHtmlAttribute(opt_data.replacedPath ? opt_data.replacedPath : '') + '" readonly></div><div class="input-group-btn"><button class="btn btn-sm btn-accent explorer-section-try-button" type="button" data-onclick="' + soy.$$escapeHtmlAttribute(opt_data.id) + ':handleClickRun_">Run</button></div></div></div></div>';
+  var methodItems__soy364 = opt_data.method ? opt_data.method : ['get'];
+  var selectedIndex__soy365 = opt_data.methodSelectedIndex ? opt_data.methodSelectedIndex : 0;
+  output += '<div class="col-md-12"><div class="input-group">' + ((methodItems__soy364.length > 1) ? '<div class="input-group-btn">' + soy.$$escapeHtml(Templates.Select.content({events: {selectedIndexChanged: opt_data.id + ':handleMethodSelectedIndexChanged_'}, id: opt_data.id + '-methodSelect', label: '', items: methodItems__soy364, buttonClass: 'btn btn-default btn-sm dropdown-select-group-left', hiddenFieldName: 'method', selectedIndex: selectedIndex__soy365}, null, opt_ijData)) + '</div>' : '<div class="input-group-addon explorer-section-try-method">' + soy.$$escapeHtml(methodItems__soy364[0]) + '</div>') + '<div class="input-inner-addon input-inner-addon-left"><button data-clipboard data-target="#explorer-section-try-input" class="explorer-section-try-icon btn-transparent icon-16-link" type="button"></button><input id="explorer-section-try-input" class="form-control" type="text" value="' + soy.$$escapeHtmlAttribute(opt_data.host ? opt_data.host : '') + soy.$$escapeHtmlAttribute(opt_data.replacedPath ? opt_data.replacedPath : '') + '" readonly></div><div class="input-group-btn"><button class="btn btn-sm btn-accent explorer-section-try-button" type="button" data-onclick="' + soy.$$escapeHtmlAttribute(opt_data.id) + ':handleClickRun_">Run</button></div></div></div></div>';
   return soydata.VERY_UNSAFE.ordainSanitizedHtml(output);
 };
 if (goog.DEBUG) {
@@ -227,12 +229,12 @@ if (goog.DEBUG) {
  */
 Templates.ApiExplorer.tryResponse = function(opt_data, opt_ignored, opt_ijData) {
   var output = '<div id="' + soy.$$escapeHtmlAttribute(opt_data.id) + '-tryResponse" class="row"><div class="explorer-section-response col-md-12' + soy.$$escapeHtmlAttribute(opt_data.response && opt_data.response.statusText ? '' : ' hidden') + '"><label class="api-section-label">Response</label><div class="explorer-status-container"><div class="row"><div class="col-md-4"><span class="explorer-status-streaming"><span class="explorer-status-streaming-pulse"></span> Streaming results...</span></div><div class="col-md-4">';
-  var statusCode__soy416 = opt_data.response ? opt_data.response.statusCode : 0;
-  var statusText__soy417 = opt_data.response ? opt_data.response.statusText : '';
-  output += '<span class="explorer-status explorer-status-' + soy.$$escapeHtmlAttribute(Math.floor(statusCode__soy416 / 100)) + 'xx">' + soy.$$escapeHtml(statusCode__soy416) + ' ' + soy.$$escapeHtml(statusText__soy417) + '</span></div>';
-  var methodItems__soy425 = opt_data.method ? opt_data.method : ['get'];
-  var selectedIndex__soy426 = opt_data.methodSelectedIndex ? opt_data.methodSelectedIndex : 0;
-  output += Templates.ApiExplorer.tryRealTime({id: opt_data.id, methodName: methodItems__soy425[selectedIndex__soy426], surfaceId: 'tryRealTime'}, null, opt_ijData) + '</div></div><div class="explorer-code-container">' + soy.$$escapeHtml(Templates.CodeMirror.content({id: opt_data.id + '-responseCodeMirror', visible: false}, null, opt_ijData)) + '</div></div></div>';
+  var statusCode__soy420 = opt_data.response ? opt_data.response.statusCode : 0;
+  var statusText__soy421 = opt_data.response ? opt_data.response.statusText : '';
+  output += '<span class="explorer-status explorer-status-' + soy.$$escapeHtmlAttribute(Math.floor(statusCode__soy420 / 100)) + 'xx">' + soy.$$escapeHtml(statusCode__soy420) + ' ' + soy.$$escapeHtml(statusText__soy421) + '</span></div>';
+  var methodItems__soy429 = opt_data.method ? opt_data.method : ['get'];
+  var selectedIndex__soy430 = opt_data.methodSelectedIndex ? opt_data.methodSelectedIndex : 0;
+  output += Templates.ApiExplorer.tryRealTime({id: opt_data.id, methodName: methodItems__soy429[selectedIndex__soy430], surfaceId: 'tryRealTime'}, null, opt_ijData) + '</div></div><div class="explorer-code-container">' + soy.$$escapeHtml(Templates.CodeMirror.content({id: opt_data.id + '-responseCodeMirror', visible: false}, null, opt_ijData)) + '</div></div></div>';
   return soydata.VERY_UNSAFE.ordainSanitizedHtml(output);
 };
 if (goog.DEBUG) {
@@ -240,7 +242,7 @@ if (goog.DEBUG) {
 }
 
 Templates.ApiExplorer.content.params = ["id"];
-Templates.ApiExplorer.title.params = ["id","method","path","title","visibility"];
+Templates.ApiExplorer.title.params = ["data","id","method","path","title","visibility"];
 Templates.ApiExplorer.description.params = ["id","description"];
 Templates.ApiExplorer.auth.params = ["id","auth"];
 Templates.ApiExplorer.params.params = ["id","parameters"];
