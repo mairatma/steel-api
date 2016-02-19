280d9bae23fd646699d36d050605c1cc40714be4
Maira Bello <maira.araujo@liferay.com>
2016-02-19 10:46:05 -0300

    Adds missing entry file

---
+++ src/all/api.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import ApiBase from '../ApiBase';
+import ApiBuilder from '../ApiBuilder';
+import ApiExplorer from '../ApiExplorer';
+
+export { ApiBase, ApiBuilder, ApiExplorer };
